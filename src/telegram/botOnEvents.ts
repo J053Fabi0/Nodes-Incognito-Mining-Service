cbdc51396920c4df6bbe21e8eabc62390afa5483
Jose Fabio <senorbinario@tuta.io>
2023-02-17 13:47:32 -0600

    Role added to the reply to any message

--- src/telegram/botOnEvents.ts
+++ src/telegram/botOnEvents.ts
@@ -2,6 +2,7 @@ import bot from "./initBot.ts";
 import handleError from "../utils/handleError.ts";
 import { lastErrorTimes } from "../utils/variables.ts";
 import getNodesStatus from "../utils/getNodesStatus.ts";
+import sendMessage from "./sendMessage.ts";
 
 bot.on("message", async (ctx) => {
   if (ctx?.chat?.id === 861616600)
@@ -9,13 +10,15 @@ bot.on("message", async (ctx) => {
       if (ctx.message?.text === "restart" || ctx.message?.text === "reset")
         for (const key of Object.keys(lastErrorTimes)) delete lastErrorTimes[key];
 
-      ctx.replyWithHTML(
-        "<b>Name: status, isSlashed</b>" +
+      await sendMessage(
+        "<b>Name: status, role, isSlashed</b>" +
           "\n<code>" +
           (await getNodesStatus())
-            .map(({ name, status, isSlashed }) => `${name}: ${status}, ${isSlashed}`)
+            .map(({ name, status, role, isSlashed }) => `${name}: ${status}, ${role}, ${isSlashed}`)
             .join("\n") +
-          "</code>"
+          "</code>",
+        ctx.chat.id,
+        { parse_mode: "HTML" }
       );
     } catch (e) {
       handleError(e);
