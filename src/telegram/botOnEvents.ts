975bfdc2313ea1b5349072fe21a1080c601f7d41
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-04-21 02:13:46 -0600

    If a command fits exactly use that one

--- src/telegram/botOnEvents.ts
+++ src/telegram/botOnEvents.ts
@@ -48,11 +48,16 @@ async function onMessage(ctx: Filter<Context, "message">) {
 
         const matchingCommands = commands.filter((c) => c.startsWith(normalizedCommand));
         if (matchingCommands.length > 1) {
-          await sendHTMLMessage(
-            `Command <code>${normalizedCommand}</code> is ambiguous. Did you mean one of these?\n- <code>` +
-              `${matchingCommands.map((c) => `${c} ${args.join(" ")}`).join("</code>\n- <code>")}</code>`
-          );
-          continue;
+          // if the command fits exactly with one of the possible commands, it is not ambiguous
+          const exactCommand = commands.find((c) => c === normalizedCommand);
+          if (exactCommand) matchingCommands.unshift(exactCommand);
+          else {
+            await sendHTMLMessage(
+              `Command <code>${normalizedCommand}</code> is ambiguous. Did you mean one of these?\n- <code>` +
+                `${matchingCommands.map((c) => `${c} ${args.join(" ")}`).join("</code>\n- <code>")}</code>`
+            );
+            continue;
+          }
         } else if (matchingCommands.length === 0) {
           await sendHTMLMessage(helpMessage);
           continue;
