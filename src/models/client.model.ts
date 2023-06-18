b50bc9eec4c484974eba9cb96515efa2237a8815
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-06-17 19:12:56 -0600

    Example of how to create indexes

--- src/models/client.model.ts
+++ src/models/client.model.ts
@@ -3,4 +3,13 @@ import Client from "../types/collections/client.type.ts";
 
 const authorModel = db.collection<Client>("clients");
 
+// Exmaple of creating indexes
+// authorModel.createIndexes({
+//   indexes: [
+//     { key: { telegram: 1 }, name: "telegram", unique: true },
+//     { key: { notionPage: 1 }, name: "notionPage", unique: true },
+//     { key: { telegram: 1, notionPage: 1 }, name: "telegram_notionPage", unique: true },
+//   ],
+// });
+
 export default authorModel;
