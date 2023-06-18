16a3dfccf22fa762fd452986edc6cca0ef5da126
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-06-17 22:46:04 -0600

    Indexes for node: client-name, client-number, paymentAddress and validatorPublic

--- src/models/node.model.ts
+++ src/models/node.model.ts
@@ -6,7 +6,12 @@ const nodeModel = db.collection<Node>("nodes");
 nodeModel.createIndexes({
   indexes: [
     { key: { client: 1 }, name: "nodes_client" },
+    { key: { client: 1, name: 1 }, name: "nodes_client_name", unique: true },
+    { key: { client: 1, number: 1 }, name: "nodes_client_number", unique: true },
+
     { key: { dockerIndex: 1 }, name: "nodes_dockerIndex", unique: true },
+    { key: { paymentAddress: 1 }, name: "nodes_paymentAddress", unique: true },
+    { key: { validatorPublic: 1 }, name: "nodes_validatorPublic", unique: true },
   ],
 });
 
