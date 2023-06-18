fee420efdfc54a3878e06c4b0838e52f48a9ef1f
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-06-17 23:17:33 -0600

    Add time to nodeEarning schema

--- src/types/collections/nodeEarning.type.ts
+++ src/types/collections/nodeEarning.type.ts
@@ -2,6 +2,7 @@ import { ObjectId } from "mongo";
 import CommonCollection from "./commonCollection.type.ts";
 
 export default interface NodeEarning extends CommonCollection {
+  time: Date;
   epoch: number;
   node: ObjectId;
   earning: number;
