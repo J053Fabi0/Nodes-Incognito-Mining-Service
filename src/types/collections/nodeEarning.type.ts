e81443a9aff7c27ad77345ba79173f226f3af402
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-06-17 20:36:12 -0600

    Add epoch to nodeEarning schema

--- src/types/collections/nodeEarning.type.ts
+++ src/types/collections/nodeEarning.type.ts
@@ -2,6 +2,7 @@ import { ObjectId } from "mongo";
 import CommonCollection from "./commonCollection.type.ts";
 
 export default interface NodeEarning extends CommonCollection {
+  epoch: number;
   node: ObjectId;
   earning: number;
 }
