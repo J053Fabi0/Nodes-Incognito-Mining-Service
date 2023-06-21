e7b3dd34a8fab37383f523ac8ce1e669d9601559
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-06-20 18:48:56 -0600

    Cake colors for bars

--- constants.ts
+++ constants.ts
@@ -46,13 +46,10 @@ export const maxDiskPercentageUsage = json.maxDiskPercentageUsage;
 export const waitingTimes = json.waitingTimes;
 
 export const BAR_COLORS = [
-  "#4dc9f6",
-  "#f67019",
-  "#f53794",
-  "#537bc4",
-  "#acc236",
-  "#166a8f",
-  "#00a950",
-  "#58595b",
-  "#8549ba",
+  //
+  "#ffb3ba",
+  "#ffdfba",
+  "#fafa9e",
+  "#baffc9",
+  "#bae1ff",
 ];
