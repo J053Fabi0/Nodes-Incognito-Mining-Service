f2dfc96406fad44e63fa4a08db67330e8416b3aa
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-06-24 16:17:44 -0600

    Introduction text with details about the service

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,14 +1,15 @@
 import { Head } from "$fresh/runtime.ts";
+import { asset } from "$fresh/runtime.ts";
 import State from "../types/state.type.ts";
 import Metas from "../components/Metas.tsx";
 import { Chart } from "fresh-charts/mod.ts";
 import { BAR_COLORS } from "../constants.ts";
 import { toFixedS } from "../utils/numbersString.ts";
-import Typography from "../components/Typography.tsx";
 import { Handlers, PageProps } from "$fresh/server.ts";
 import { countNodes } from "../controllers/node.controller.ts";
 import getNodesStatistics from "../utils/getNodesStatistics.ts";
 import { countNodeEarnings } from "../controllers/nodeEarning.controller.ts";
+import Typography, { getTypographyClass } from "../components/Typography.tsx";
 
 const styles = {
   td: "border border-slate-300 py-2 px-3",
@@ -47,30 +48,46 @@ export default function Home({ data }: PageProps<HomeProps>) {
     <>
       <Head>
         <Metas title="Hosting nodes Incognito" description="Incognito nodes service" />
+        <link href={asset("/styles/checkUL.css")} rel="stylesheet" />
       </Head>
 
-      <Typography variant="h1" class="mt-3 mb-10">
+      <Typography variant="h1" class="mt-3 mb-5">
         Hosting service for Incognito nodes
+      </Typography>
+
+      <ul class="list-disc list-inside checkUL mb-5">
+        <li class={getTypographyClass("lead")}>You provide the stake, we the infrastructure.</li>
+        <li class={getTypographyClass("lead")}>
+          Earnings and stake in your control, we don't ask for any private keys.
+        </li>
+        <li class={getTypographyClass("lead")}>
+          Monthly fee is based on earnings: 10%, no more.
+          <li class={getTypographyClass("lead")}>5 USD for each node's initial setup.</li>
+        </li>
+      </ul>
+
+      <Typography variant="h2" class="mb-5">
+        Some statistics
       </Typography>
 
       <table class="table-auto border-collapse border border-slate-400 mb-5">
         <tbody>
           <tr>
             <td class={styles.td}>
-              <Typography variant="h3">Hosted nodes</Typography>
+              <Typography variant="h4">Hosted nodes</Typography>
             </td>
             <td class={styles.td}>
-              <Typography variant="h3">
+              <Typography variant="h4">
                 <code>{nodesCount}</code>
               </Typography>
             </td>
           </tr>
           <tr>
             <td class={styles.td}>
-              <Typography variant="h3">Total earnings count</Typography>
+              <Typography variant="h4">Total earnings count</Typography>
             </td>
             <td class={styles.td}>
-              <Typography variant="h3">
+              <Typography variant="h4">
                 <code>{earningsCount}</code>
               </Typography>
             </td>
