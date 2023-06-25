2d71406df5a40d608d85ac1f782ff982cd012902
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-06-24 23:25:20 -0600

    Allow some admin url for localhost

--- utils/isAdminPage.tsx
+++ utils/isAdminPage.tsx
@@ -3,9 +3,19 @@ export const adminURLs = [
   "/credentials",
 ].map((pathname) => new URLPattern({ pathname }));
 
+export const allowedForLocalhost = [
+  //
+  "/credentials",
+].map((pathname) => new URLPattern({ pathname }));
+
 /**
  * @param url The full URL of the page, including the domain name and protocol
  */
 export default function isAdminPage(url: string) {
+  const urlObj = new URL(url);
+
+  // localhost is allowed for development for some pages
+  if (urlObj.hostname === "localhost" && allowedForLocalhost.some((pattern) => pattern.test(url))) return false;
+
   return adminURLs.some((pattern) => pattern.test(url));
 }
