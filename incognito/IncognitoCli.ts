dc6d18c78a59028de48be0cea7721e77c32d094f
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-06-25 21:51:39 -0600

    Add PRV_ID property to IncognitoCli class

--- incognito/IncognitoCli.ts
+++ incognito/IncognitoCli.ts
@@ -1,9 +1,14 @@
 import generateAccount from "./generateAccount.ts";
 import { binaryWrapper } from "duplicatedFilesCleanerIncognito";
 
+const PRV_ID = "0000000000000000000000000000000000000000000000000000000000000004" as const;
+
 export default class IncognitoCli {
   privateKey?: string;
   incognitoCli = binaryWrapper("incognito-cli");
+  PRV_ID = PRV_ID;
+
+  static PRV_ID = PRV_ID;
 
   constructor(privateKey?: string) {
     this.privateKey = privateKey;
