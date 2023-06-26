032c2b4804362e8c4884b895089d329aea9b3b0d
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-06-26 10:11:03 -0600

    Option to use or not decimal format when checking the balance

--- incognito/account/balance.ts
+++ incognito/account/balance.ts
@@ -4,12 +4,21 @@ interface BalanceAccount {
   Balance: number;
 }
 
-export default async function balanceAccount(this: IncognitoCli, tokenId: string = this.PRV_ID) {
+interface Options {
+  tokenId?: string;
+  /** Default: true */
+  decimalFormat?: boolean;
+}
+
+export default async function balanceAccount(this: IncognitoCli, options: Options = {}) {
   if (!this.privateKey) throw new Error("privateKey is not set");
 
-  const args = ["account", "balance", "--privateKey", this.privateKey, "--tokenID", tokenId];
+  if (options.decimalFormat === undefined) options.decimalFormat = true;
+  if (options.tokenId === undefined) options.tokenId = this.PRV_ID;
+
+  const args = ["account", "balance", "--privateKey", this.privateKey, "--tokenID", options.tokenId];
 
   const { Balance } = JSON.parse(await this.incognitoCli(args)) as BalanceAccount;
 
-  return Balance;
+  return options.decimalFormat ? Balance / 1e9 : Balance;
 }
