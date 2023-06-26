8272b1ad32d9c8aea728a4a6ce774f249751060b
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-06-25 21:11:30 -0600

    Fix: startOf month for getNodesStatistics

--- utils/getNodesStatistics.ts
+++ utils/getNodesStatistics.ts
@@ -15,6 +15,7 @@ export default async function getNodesStatistics() {
     .map((_, i) =>
       dayjs()
         .subtract(i + 1, "month")
+        .startOf("month")
         .toDate()
     )
     .reverse();
