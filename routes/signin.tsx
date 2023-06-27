77fc28e8c96e3eaafad595f04eb382e5d6eab688
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-06-27 14:00:53 -0600

    /signin title change and different title if prop "create" is present

--- routes/signin.tsx
+++ routes/signin.tsx
@@ -1,8 +1,9 @@
 import State from "../types/state.type.ts";
-import { Handlers } from "$fresh/server.ts";
 import redirect from "../utils/redirect.ts";
 import cryptr from "../utils/cryptrInstance.ts";
 import { checkSignature } from "grammy_validator";
+import Typography from "../components/Typography.tsx";
+import { Handlers, PageProps } from "$fresh/server.ts";
 import getQueryParams from "../utils/getQueryParams.ts";
 import IncognitoCli from "../incognito/IncognitoCli.ts";
 import { isTelegramPayload } from "../types/telegramPayload.type.ts";
@@ -10,7 +11,11 @@ import { createAccount } from "../controllers/account.controller.ts";
 import { createClient, getClient } from "../controllers/client.controller.ts";
 import { NOTIFICATIONS_BOT_TOKEN, NOTIFICATIONS_BOT_USERNAME } from "../env.ts";
 
-export const handler: Handlers<undefined, State> = {
+interface SigninProps {
+  creating: boolean;
+}
+
+export const handler: Handlers<SigninProps, State> = {
   async GET(req, ctx) {
     if (ctx.state.user) return redirect("/");
 
@@ -55,29 +60,41 @@ export const handler: Handlers<undefined, State> = {
       return redirect("/");
     }
 
-    return ctx.render();
+    return ctx.render({ creating: "create" in params });
   },
 };
 
-export default function SignIn() {
+export default function SignIn({ data }: PageProps<SigninProps>) {
+  const { creating } = data;
+
   return (
-    <div class="w-full flex flex-col flex-wrap items-center mt-10">
-      <div
-        class={
-          "border-solid border-cyan-300 border-2 rounded-full p-1 " +
-          "bg-gradient-to-r from-cyan-500 to-blue-500 " +
-          "min-h-[50.8px] min-w-[250px]"
-        }
-      >
-        <script
-          async
-          data-size="large"
-          data-auth-url="signin"
-          data-request-access="write"
-          src="https://telegram.org/js/telegram-widget.js?22"
-          data-telegram-login={NOTIFICATIONS_BOT_USERNAME.toLowerCase()}
-        />
+    <>
+      <Typography variant="h1" class="mb-5">
+        {creating ? "Create an account with Telegram" : "Sign in or create an account"}
+      </Typography>
+
+      <Typography variant="h4" class="mb-5">
+        {creating ? "Start hosting your nodes with us today" : "Both with the same button"}.
+      </Typography>
+
+      <div class="w-full flex flex-col flex-wrap items-center mt-10">
+        <div
+          class={
+            "border-solid border-cyan-300 border-2 rounded-full p-1 " +
+            "bg-gradient-to-r from-cyan-500 to-blue-500 " +
+            "min-h-[50.8px] min-w-[250px]"
+          }
+        >
+          <script
+            async
+            data-size="large"
+            data-auth-url="signin"
+            data-request-access="write"
+            src="https://telegram.org/js/telegram-widget.js?22"
+            data-telegram-login={NOTIFICATIONS_BOT_USERNAME.toLowerCase()}
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
