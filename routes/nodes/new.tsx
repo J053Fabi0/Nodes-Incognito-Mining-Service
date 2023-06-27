4bafc9e1d7715d0f5cb71cd0c70c9ef9707b1f45
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-06-27 15:47:35 -0600

    Title for /nodes/new

--- routes/nodes/new.tsx
+++ routes/nodes/new.tsx
@@ -4,6 +4,9 @@ export default function IndividualNode() {
   return (
     <>
       <Typography variant="h1" class="mt-3 mb-5">
+        Host a new node
+      </Typography>
+      <Typography variant="h3" class="mt-3 mb-5">
         Coming soon
       </Typography>
     </>
