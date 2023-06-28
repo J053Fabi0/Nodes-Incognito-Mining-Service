d13c96e6d3420f4a4d23dcba031f032ab1d21e2b
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-06-27 21:17:10 -0600

    EventedArray fix: return types for NoEvent methods

--- utils/EventedArray.ts
+++ utils/EventedArray.ts
@@ -24,41 +24,41 @@ export default class EventedArray<Type = any> extends Array {
     this.handler = handler;
   }
 
-  push(...items: Type[]) {
+  push(...items: Type[]): number {
     super.push(...items);
     this.handler({ array: this, added: items, removed: null, method: "push" });
     return this.length;
   }
-  pushNoEvent(...items: Type[]) {
+  pushNoEvent(...items: Type[]): number {
     return super.push(...items);
   }
 
-  pop() {
+  pop(): Type | undefined {
     const length = this.length; // this.length gets the length before the pop. Using super.length gets the length after the pop.
     const removed = length > 0 ? [super.pop() as Type] : [];
     this.handler({ array: this, added: null, removed, method: "pop" });
     return length > 0 ? removed[0] : undefined;
   }
-  popNoEvent() {
+  popNoEvent(): Type | undefined {
     return super.pop();
   }
 
-  unshift(...items: Type[]) {
+  unshift(...items: Type[]): number {
     super.unshift(...items);
     this.handler({ array: this, added: items, removed: null, method: "unshift" });
     return this.length;
   }
-  unshiftNoEvent(...items: Type[]) {
+  unshiftNoEvent(...items: Type[]): number {
     return super.unshift(...items);
   }
 
-  shift() {
+  shift(): Type | undefined {
     const length = this.length; // this.length gets the length before the shift. Using super.length gets the length after the shift.
     const removed = length > 0 ? [super.shift() as Type] : [];
     this.handler({ array: this, added: null, removed, method: "shift" });
     return length > 0 ? removed[0] : undefined;
   }
-  shiftNoEvent() {
+  shiftNoEvent(): Type | undefined {
     return super.shift();
   }
 
@@ -77,14 +77,14 @@ export default class EventedArray<Type = any> extends Array {
     return typeof deleteCount === "undefined" ? super.splice(start) : super.splice(start, deleteCount, ...items);
   }
 
-  get lengths() {
+  get lengths(): number {
     return this.length;
   }
   set lengths(value: number) {
     const removed = super.splice(value);
     this.handler({ array: this, added: null, removed, method: "length" });
   }
-  get lengthNoEvent() {
+  get lengthNoEvent(): number {
     return this.length;
   }
   set lengthNoEvent(value: number) {
@@ -94,7 +94,7 @@ export default class EventedArray<Type = any> extends Array {
   /**
    * @deprecated Use `lengths` instead, to trigger the handler.
    */
-  get length() {
+  get length(): number {
     return this.length;
   }
   set length(value: number) {
