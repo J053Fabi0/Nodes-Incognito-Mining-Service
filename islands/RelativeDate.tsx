0f54b2eecc5a1797d29dcb697cb2d64324feb5f5
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-07-02 15:45:12 -0600

    Make RelativeDate update every second

--- islands/RelativeDate.tsx
+++ islands/RelativeDate.tsx
@@ -1,4 +1,5 @@
 import dayjs from "dayjs/mod.ts";
+import { useSignal } from "@preact/signals";
 import capitalizeFn from "../utils/capitalize.ts";
 import relativeTime from "dayjs/plugin/relativeTime.ts";
 
@@ -11,6 +12,14 @@ interface RelativeDateProps {
 }
 
 export default function RelativeDate({ date, capitalize }: RelativeDateProps) {
-  const t = dayjs(date).fromNow();
-  return <>{capitalize ? capitalizeFn(t) : t}.</>;
+  const interval = useSignal<number | null>(null);
+  const t = useSignal<string>(dayjs(date).fromNow());
+
+  if (interval.value === null)
+    interval.value = setInterval(() => {
+      interval.value = null;
+      t.value = dayjs(date).fromNow();
+    }, 1_000);
+
+  return <>{capitalize ? capitalizeFn(t.value) : t.value}.</>;
 }
