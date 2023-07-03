c8bae196ae77115d2d01f73b15c35b88c626e30d
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-07-03 11:46:28 -0600

    Handle errors in deleteDocker and deleteNginxConfig

--- incognito/docker/deleteDocker.ts
+++ incognito/docker/deleteDocker.ts
@@ -1,11 +1,12 @@
 import { dataDir } from "./createDocker.ts";
 import { docker } from "../../utils/commands.ts";
+import handleError from "../../utils/handleError.ts";
 
 /**
  * Stops and removes a docker container. Removes also the data directory.
  */
 export default async function deleteDocker(dockerIndex: number) {
-  await docker(["stop", `inc_mainnet_${dockerIndex}`]).catch(() => {});
-  await docker(["rm", `inc_mainnet_${dockerIndex}`]).catch(() => {});
-  await Deno.remove(`${dataDir}_${dockerIndex}`, { recursive: true }).catch(() => {});
+  await docker(["stop", `inc_mainnet_${dockerIndex}`]).catch(handleError);
+  await docker(["rm", `inc_mainnet_${dockerIndex}`]).catch(handleError);
+  await Deno.remove(`${dataDir}_${dockerIndex}`, { recursive: true }).catch(handleError);
 }
--- incognito/nginx/deleteNginxConfig.ts
+++ incognito/nginx/deleteNginxConfig.ts
@@ -2,6 +2,7 @@ import { ObjectId } from "mongo/mod.ts";
 import { join } from "std/path/mod.ts";
 import { systemctl } from "../../utils/commands.ts";
 import { sitesAvailable, sitesEnabled } from "./createNginxConfig.ts";
+import handleError from "../../utils/handleError.ts";
 
 /**
  * @param number The node number of the owner, not docker index
@@ -9,8 +10,8 @@ import { sitesAvailable, sitesEnabled } from "./createNginxConfig.ts";
 export default async function deleteNginxConfig(clientId: string | ObjectId, number: number) {
   const fileName = `${number}-${clientId}`.toLowerCase();
 
-  await Deno.remove(join(sitesAvailable, fileName));
-  await Deno.remove(join(sitesEnabled, fileName));
+  await Deno.remove(join(sitesEnabled, fileName)).catch(handleError);
+  await Deno.remove(join(sitesAvailable, fileName)).catch(handleError);
 
   await systemctl(["reload", "nginx"]);
 }
