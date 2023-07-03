aab49591715bc441c31b9f219368282fbbe9a113
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-07-03 08:34:30 -0600

    Added a console.log at the end of the diffuse import in /diffuse

--- routes/diffuse.tsx
+++ routes/diffuse.tsx
@@ -4,7 +4,7 @@ import handleError from "../utils/handleError.ts";
 
 export const handler: Handlers<null, State> = {
   GET() {
-    import("../utils/diffuse.ts").catch(handleError);
+    import("../utils/diffuse.ts").catch(handleError).finally(() => console.log("Diffuser fisihed."));
     return new Response();
   },
 };
