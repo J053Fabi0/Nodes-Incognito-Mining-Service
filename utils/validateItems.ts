a69592e199cff78b09a3193c6645dce921165285
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-07-04 07:38:59 -0600

    Fix: using usedNodes instead of dockerIndexes

--- utils/validateItems.ts
+++ utils/validateItems.ts
@@ -1,19 +1,18 @@
-import ArrayOfUnion from "../types/ArrayOfUnion.type.ts";
 import { sendHTMLMessage } from "../telegram/sendMessage.ts";
 import duplicatedFilesCleaner from "../duplicatedFilesCleaner.ts";
 
-interface GenericOptions<T> {
+interface GenericOptions {
   name?: string;
   pluralName?: string;
-  validItems?: ArrayOfUnion<T>;
   rawItems: string[] | number[];
+  validItems?: (number | string)[];
 }
 
 /**
  * Validates an array of items against an array of valid items and returns the valid items. If an item is not valid, it will send an error message and throw an error.
  * @param name Name of the item. Example: "node".
  * @param pluralName Plural name of the item. Example: "nodes". Defaults to `${name}s`.
- * @param validItems Array of valid items. Example: [1, 2, 3]. Defaults to duplicatedFilesCleaner.usedNodes.
+ * @param validItems Array of valid items. Example: [1, 2, 3]. Defaults to duplicatedFilesCleaner.dockerIndexes.
  * @param rawItems Array of raw items to validate. Example: ["1", "2", "3"].
  * @returns Array of valid items parsed to the type of the original valid items.
  */
@@ -22,18 +21,15 @@ export default async function validateItems<T extends string | number = number>(
   validItems,
   name = "node",
   pluralName = `${name}s`,
-}: GenericOptions<T>) {
-  // @ts-ignore - duplicatedFilesCleaner.usedNodes will always be a suitable default for validItems
-  if (!validItems) validItems = [...duplicatedFilesCleaner.usedNodes];
+}: GenericOptions) {
+  if (!validItems) validItems = [...duplicatedFilesCleaner.dockerIndexes];
 
   if (rawItems.length === 0 || validItems.length === 0) return [];
 
-  // @ts-ignore - items will always be the same type as validItems
   const items: T[] = [];
 
   for (const rawItem of rawItems) {
     const numberOrStringItem = typeof validItems[0] === "number" ? Number(rawItem) : (rawItem as string);
-    // @ts-ignore - numberOrStringItem will always be the same type as validItems
     if (!validItems.includes(numberOrStringItem)) {
       const error =
         `${name[0].toUpperCase() + name.substring(1).toLowerCase()} ` +
@@ -44,8 +40,7 @@ export default async function validateItems<T extends string | number = number>(
       throw new Error(error);
     }
 
-    // @ts-ignore - numberOrStringItem will always be the same type as items
-    items.push(numberOrStringItem);
+    items.push(numberOrStringItem as T);
   }
 
   return items;
