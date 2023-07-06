822cae5455cd17781dd87822a1815691f4390be5
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-07-06 12:34:37 -0600

    Catch bots errors and console.error them

--- telegram/initBots.ts
+++ telegram/initBots.ts
@@ -11,11 +11,15 @@ bot.on("message", (ctx) => {
   if (ctx.message.text && ctx?.chat?.id === ADMIN_ID) submitCommand(ctx.message.text);
 });
 
+bot.catch(console.error);
+
 bot.start();
 
 // Initialize the notifications bot
 export const notificationsBot = new Bot(NOTIFICATIONS_BOT_TOKEN);
 
 notificationsBot.on("message", (ctx) => void ctx.reply(`<code>${ctx.chat?.id}</code>`, { parse_mode: "HTML" }));
+
+notificationsBot.catch(console.error);
 
 notificationsBot.start();
