d81c744441122f4d7b035aea4ff47310289969a1
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-07-09 16:11:05 -0600

    Set utcOffset to 0 in crons

--- crons/crons.ts
+++ crons/crons.ts
@@ -1,5 +1,5 @@
-import Cron from "croner";
 import checkNodes from "./checkNodes.ts";
+import Cron, { CronOptions } from "croner";
 import checkEarnings from "./checkEarnings.ts";
 import handleError from "../utils/handleError.ts";
 import checkAccounts, { Unit } from "./checkAccounts.ts";
@@ -7,20 +7,22 @@ import deleteEmptySessions from "./deleteEmptySessions.ts";
 import checkNotStakedNodes from "./checkNotStakedNodes.ts";
 import cacheNodesStatistics from "./cacheNodesStatistics.ts";
 
-new Cron("*/5 * * * *", { protect: true, catch: handleError }, checkEarnings);
+const options: CronOptions = { catch: handleError, utcOffset: 0 };
 
-new Cron("*/1 * * * *", { protect: true, catch: handleError }, checkNodes);
+new Cron("*/5 * * * *", { protect: true, ...options }, checkEarnings);
+
+new Cron("*/1 * * * *", { protect: true, ...options }, checkNodes);
 
 // every minute, check the accounts that have used the website in the last 5 minutes
-new Cron("*/1 * * * *", { protect: true, catch: handleError }, checkAccounts.bind(null, 5, Unit.minute));
+new Cron("*/1 * * * *", { protect: true, ...options }, checkAccounts.bind(null, 5, Unit.minute));
 // every hour, check the accounts that have used the website in the last day
-new Cron("0 * * * *", { protect: true, catch: handleError }, checkAccounts.bind(null, 1, Unit.day));
+new Cron("0 * * * *", { protect: true, ...options }, checkAccounts.bind(null, 1, Unit.day));
 
 // delete empty sessions every hour
-new Cron("0 * * * *", { protect: true, catch: handleError }, deleteEmptySessions);
+new Cron("0 * * * *", { protect: true, ...options }, deleteEmptySessions);
 
 // check not staked nodes every 30 minutes
-new Cron("*/30 * * * *", { protect: true, catch: handleError }, checkNotStakedNodes);
+new Cron("*/30 * * * *", { protect: true, ...options }, checkNotStakedNodes);
 
 // cache nodes statistics every 5 minutes
-new Cron("*/5 * * * *", { catch: handleError }, cacheNodesStatistics);
+new Cron("*/5 * * * *", options, cacheNodesStatistics);
