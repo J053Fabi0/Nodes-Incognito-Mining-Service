67e54c9843fe6a0a45319388d494d07602a48b32
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-07-09 12:38:36 -0600

    Fix: autoMove was not working

--- crons/checkNodes.ts
+++ crons/checkNodes.ts
@@ -10,15 +10,13 @@ import flags from "../utils/flags.ts";
 import { escapeHtml } from "escapeHtml";
 import getNodesStatus from "../utils/getNodesStatus.ts";
 import isBeingIgnored from "../utils/isBeingIgnored.ts";
+import submitCommand from "../telegram/submitCommand.ts";
 import handleNodeError from "../utils/handleNodeError.ts";
-import handleInfo from "../telegram/handlers/handleInfo.ts";
 import { sendHTMLMessage } from "../telegram/sendMessage.ts";
-import handleDelete from "../telegram/handlers/handleDelete.ts";
 import getShouldBeOffline from "../utils/getShouldBeOffline.ts";
 import getMinutesSinceError from "../utils/getMinutesSinceError.ts";
 import { waitingTimes, maxDiskPercentageUsage } from "../constants.ts";
 import { df, docker, dockerPs } from "duplicatedFilesCleanerIncognito";
-import handleCopyOrMove from "../telegram/handlers/handleCopyOrMove.ts";
 import handleTextMessage from "../telegram/handlers/handleTextMessage.ts";
 import getInstructionsToMoveOrDelete from "../utils/getInstructionsToMoveOrDelete.ts";
 import duplicatedFilesCleaner, { duplicatedConstants } from "../duplicatedFilesCleaner.ts";
@@ -132,18 +130,14 @@ export default async function checkNodes() {
       for (const instruction of instructionsToMoveOrDelete) {
         const nodeTo = nodesStatus.find((node) => `${node.dockerIndex}` === instruction.to);
         const nodeFrom = nodesStatus.find((node) => `${node.dockerIndex}` === instruction.from);
-        const dockerStatusTo = nodeTo ? dockerStatuses[nodeTo.dockerIndex].status : "OFFLINE";
-        const dockerStatusFrom = nodeFrom ? dockerStatuses[nodeFrom.dockerIndex].status : "OFFLINE";
+        const isToRunning = nodeTo ? dockerStatuses[nodeTo.dockerIndex].running : true;
+        const isFromRunning = nodeFrom ? dockerStatuses[nodeFrom.dockerIndex].running : true;
 
         if (instruction.action === "move") {
-          if (dockerStatusTo === "OFFLINE" && dockerStatusFrom === "OFFLINE")
-            await handleCopyOrMove([instruction.from, instruction.to, ...instruction.shards], "move", {
-              disable_notification: true,
-            });
-        } else if (dockerStatusTo === "OFFLINE")
-          await handleDelete([instruction.from, ...instruction.shards], { disable_notification: true });
+          if (!isToRunning && !isFromRunning)
+            submitCommand(`move ${instruction.from} ${instruction.to} ${instruction.shards.join(" ")}`);
+        } else if (!isToRunning) submitCommand(`delete ${instruction.from} ${instruction.shards.join(" ")}`);
       }
-      await handleInfo(undefined, { disable_notification: true });
     }
   }
 }
