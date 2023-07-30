17ecd48244d7a37e38a654f73571ea6ee0104294
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-07-30 11:36:22 -0600

    /charts/monthly-earnings to deliver the chart as an image

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -13,23 +13,24 @@ import * as $7 from "./routes/admin/nodes.tsx";
 import * as $8 from "./routes/api/balance.tsx";
 import * as $9 from "./routes/api/variables.ts";
 import * as $10 from "./routes/bot-is-blocked.tsx";
-import * as $11 from "./routes/credentials.tsx";
-import * as $12 from "./routes/diffuse.tsx";
-import * as $13 from "./routes/index.tsx";
-import * as $14 from "./routes/me/index.tsx";
-import * as $15 from "./routes/me/transactions.tsx";
-import * as $16 from "./routes/nodes/delete/[indexOrNumber].tsx";
-import * as $17 from "./routes/nodes/delete/index.tsx";
-import * as $18 from "./routes/nodes/earnings/[number].tsx";
-import * as $19 from "./routes/nodes/earnings/index.tsx";
-import * as $20 from "./routes/nodes/earnings/monthly.tsx";
-import * as $21 from "./routes/nodes/index.tsx";
-import * as $22 from "./routes/nodes/monitor.tsx";
-import * as $23 from "./routes/nodes/new-confirm.tsx";
-import * as $24 from "./routes/nodes/new.tsx";
-import * as $25 from "./routes/nodes/notifications.tsx";
-import * as $26 from "./routes/signin.tsx";
-import * as $27 from "./routes/signout.tsx";
+import * as $11 from "./routes/charts/monthly-earnings.tsx";
+import * as $12 from "./routes/credentials.tsx";
+import * as $13 from "./routes/diffuse.tsx";
+import * as $14 from "./routes/index.tsx";
+import * as $15 from "./routes/me/index.tsx";
+import * as $16 from "./routes/me/transactions.tsx";
+import * as $17 from "./routes/nodes/delete/[indexOrNumber].tsx";
+import * as $18 from "./routes/nodes/delete/index.tsx";
+import * as $19 from "./routes/nodes/earnings/[number].tsx";
+import * as $20 from "./routes/nodes/earnings/index.tsx";
+import * as $21 from "./routes/nodes/earnings/monthly.tsx";
+import * as $22 from "./routes/nodes/index.tsx";
+import * as $23 from "./routes/nodes/monitor.tsx";
+import * as $24 from "./routes/nodes/new-confirm.tsx";
+import * as $25 from "./routes/nodes/new.tsx";
+import * as $26 from "./routes/nodes/notifications.tsx";
+import * as $27 from "./routes/signin.tsx";
+import * as $28 from "./routes/signout.tsx";
 import * as $$0 from "./islands/Balance.tsx";
 import * as $$1 from "./islands/EarningsTable.tsx";
 import * as $$2 from "./islands/EarningsTableAndOptions.tsx";
@@ -54,23 +55,24 @@ const manifest = {
     "./routes/api/balance.tsx": $8,
     "./routes/api/variables.ts": $9,
     "./routes/bot-is-blocked.tsx": $10,
-    "./routes/credentials.tsx": $11,
-    "./routes/diffuse.tsx": $12,
-    "./routes/index.tsx": $13,
-    "./routes/me/index.tsx": $14,
-    "./routes/me/transactions.tsx": $15,
-    "./routes/nodes/delete/[indexOrNumber].tsx": $16,
-    "./routes/nodes/delete/index.tsx": $17,
-    "./routes/nodes/earnings/[number].tsx": $18,
-    "./routes/nodes/earnings/index.tsx": $19,
-    "./routes/nodes/earnings/monthly.tsx": $20,
-    "./routes/nodes/index.tsx": $21,
-    "./routes/nodes/monitor.tsx": $22,
-    "./routes/nodes/new-confirm.tsx": $23,
-    "./routes/nodes/new.tsx": $24,
-    "./routes/nodes/notifications.tsx": $25,
-    "./routes/signin.tsx": $26,
-    "./routes/signout.tsx": $27,
+    "./routes/charts/monthly-earnings.tsx": $11,
+    "./routes/credentials.tsx": $12,
+    "./routes/diffuse.tsx": $13,
+    "./routes/index.tsx": $14,
+    "./routes/me/index.tsx": $15,
+    "./routes/me/transactions.tsx": $16,
+    "./routes/nodes/delete/[indexOrNumber].tsx": $17,
+    "./routes/nodes/delete/index.tsx": $18,
+    "./routes/nodes/earnings/[number].tsx": $19,
+    "./routes/nodes/earnings/index.tsx": $20,
+    "./routes/nodes/earnings/monthly.tsx": $21,
+    "./routes/nodes/index.tsx": $22,
+    "./routes/nodes/monitor.tsx": $23,
+    "./routes/nodes/new-confirm.tsx": $24,
+    "./routes/nodes/new.tsx": $25,
+    "./routes/nodes/notifications.tsx": $26,
+    "./routes/signin.tsx": $27,
+    "./routes/signout.tsx": $28,
   },
   islands: {
     "./islands/Balance.tsx": $$0,
---
+++ routes/charts/monthly-earnings.tsx
@@ -0,0 +1,27 @@
+import { BAR_COLORS } from "../../constants.ts";
+import { type Handlers } from "$fresh/server.ts";
+import { renderChart } from "fresh-charts/mod.ts";
+import { nodesStatistics } from "../../utils/variables.ts";
+
+export const handler: Handlers = {
+  GET() {
+    const { averageTotalEarningsByMonth, monthsLabels, nodesCount, earningsCount } = nodesStatistics;
+
+    return renderChart({
+      type: "bar",
+      data: {
+        labels: monthsLabels,
+        datasets: [
+          {
+            backgroundColor: BAR_COLORS,
+            label: `Average monthly earnings per node.`,
+            data: Object.values(averageTotalEarningsByMonth),
+          },
+        ],
+      },
+      options: {
+        devicePixelRatio: 1,
+      },
+    });
+  },
+};
