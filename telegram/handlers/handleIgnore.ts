e2537209a576d89648ea386424c58efcdececdc9
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-07-31 00:51:04 -0600

    Fix handleIgnore: turn type into the correct string

--- telegram/handlers/handleIgnore.ts
+++ telegram/handlers/handleIgnore.ts
@@ -2,16 +2,28 @@ import { ignore } from "../../utils/variables.ts";
 import sendMessage, { sendHTMLMessage } from "../sendMessage.ts";
 import { CommandOptions, CommandResponse } from "../submitCommandUtils.ts";
 
-type Type = (typeof errorKeys)[number] | "all";
 const errorKeys = Object.keys(ignore).sort((a, b) => a.length - b.length) as (keyof typeof ignore)[];
+type Type = (typeof errorKeys)[number] | "all";
 
 function isAll(type: string): type is "all" {
   return type.toLowerCase() === "all";
 }
 
+function getType(type: string): Type | null {
+  for (const t of [...errorKeys, "all"] as Type[]) if (type.toLowerCase() === t.toLowerCase()) return t;
+  return null;
+}
+
+async function sendError(options?: CommandOptions): Promise<CommandResponse> {
+  const error = `Valid types:\n- <code>${["all", ...errorKeys].join("</code>\n- <code>")}</code>`;
+  if (options?.telegramMessages)
+    await sendHTMLMessage(error, undefined, { disable_notification: options?.silent });
+  return { successful: false, error };
+}
+
 export default async function handleIgnore(args: string[], options?: CommandOptions): Promise<CommandResponse> {
   let number = 0; // default value is 0, to disable the ignore
-  let type: Type = "docker";
+  let type: Type | null = "docker";
 
   if (args.length === 1) {
     // the first argument could be the type if it's not a number
@@ -23,6 +35,9 @@ export default async function handleIgnore(args: string[], options?: CommandOpti
     number = Number(args[1]) || number;
   }
 
+  type = getType(type);
+  if (type === null) return sendError(options);
+
   if (number < 0) {
     const error = "The number of minutes must be positive.";
     if (options?.telegramMessages) await sendMessage(error, undefined, { disable_notification: options?.silent });
@@ -32,12 +47,8 @@ export default async function handleIgnore(args: string[], options?: CommandOpti
   if (
     (type.toLowerCase() !== "all" && !errorKeys.map((a) => a.toLowerCase()).includes(type.toLowerCase())) ||
     type.toLowerCase() === "codes"
-  ) {
-    const error = `Valid types:\n- <code>${["all", ...errorKeys].join("</code>\n- <code>")}</code>`;
-    if (options?.telegramMessages)
-      await sendHTMLMessage(error, undefined, { disable_notification: options?.silent });
-    return { successful: false, error };
-  }
+  )
+    return sendError(options);
 
   for (const t of isAll(type) ? errorKeys : [type]) {
     ignore[t].from = Date.now();
