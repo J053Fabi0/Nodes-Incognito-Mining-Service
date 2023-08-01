30cc58761af4417e979abb23ea43662325290bad
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-08-01 09:00:28 -0600

    getBlockchainInfo and getSyncState

---
+++ incognito/getBlockchainInfo.ts
@@ -0,0 +1,55 @@
+import axiod from "axiod";
+import { ShardsStr } from "duplicatedFilesCleanerIncognito";
+
+export interface BestBlockRaw {
+  Height: number;
+  Hash: string;
+  TotalTxs: number;
+  BlockProducer: string;
+  ValidationData: string;
+  /** Only beacon shard has this value not set to 0 */
+  Epoch: number;
+  Time: number;
+  /** Only beacon shard has this value not set to 0 */
+  RemainingBlockEpoch: number;
+  /** Only beacon shard has this value not set to 0 */
+  EpochBlock: number;
+}
+
+export interface BlockchainInfoRaw {
+  ChainName: string;
+  BestBlocks: Record<ShardsStr | "-1", BestBlockRaw>;
+  ActiveShards: number;
+}
+
+interface MethodResponse {
+  Id: number;
+  Result: BlockchainInfoRaw;
+  Error: null;
+  Params: "";
+  Method: "getblockchaininfo";
+  Jsonrpc: "1.0";
+}
+
+const minRequestInterval = 12_000; // 5 seconds
+// The key is the mpk
+let lastRequests: { time: number; result: BlockchainInfoRaw | null } | undefined;
+
+const postData = { jsonrpc: "1.0", method: "getblockchaininfo", params: "", id: 2 };
+
+export default async function getBlockchainInfo(): Promise<BlockchainInfoRaw | null> {
+  const lastRequest = lastRequests;
+  if (lastRequest && Date.now() - lastRequest.time < minRequestInterval && lastRequest.result)
+    return lastRequest.result;
+
+  const { data } = await axiod.post<MethodResponse | Record<string, never>>(
+    "https://mainnet.incognito.org/fullnode",
+    postData
+  );
+
+  const result = "Result" in data ? (data as MethodResponse).Result : null;
+
+  lastRequests = { time: Date.now(), result };
+
+  return result;
+}
---
+++ incognito/getSyncState.ts
@@ -0,0 +1,37 @@
+import axiod from "axiod";
+import { ShardsStr } from "duplicatedFilesCleanerIncognito";
+
+export interface SyncStateRaw {
+  IsSync: boolean;
+  LastInsert: string;
+  BlockHeight: number;
+  BlockTime: string;
+  BlockHash: string;
+}
+
+export interface SyncStateResponseRaw {
+  Beacon: SyncStateRaw;
+  Shard: Record<ShardsStr, SyncStateRaw>;
+}
+
+const minRequestInterval = 12_000; // 5 seconds
+// The key is the mpk
+const lastRequests: Record<string, { time: number; result: SyncStateResponseRaw | null } | undefined> = {};
+
+/** @param mpk The public validator key */
+export default async function getSyncState(mpk: string): Promise<SyncStateResponseRaw | null> {
+  const lastRequest = lastRequests[mpk];
+  if (lastRequest && Date.now() - lastRequest.time < minRequestInterval && lastRequest.result)
+    return lastRequest.result;
+
+  const { data } = await axiod.post<SyncStateResponseRaw | Record<string, never>>(
+    "https://monitor.incognito.org/pubkeystat/sync",
+    { mpk }
+  );
+
+  const result = "Beacon" in data ? (data as SyncStateResponseRaw) : null;
+
+  lastRequests[mpk] = { time: Date.now(), result };
+
+  return result;
+}
