4a5a67de41c00a6b0fb8f9a05ca8d1e482fa7ba2
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-08-10 20:53:08 -0600

    Set startedAt or finishedAt when changing the cache of a node running value

--- utils/setCache.ts
+++ utils/setCache.ts
@@ -8,13 +8,15 @@ type Keys = "docker.running";
 
 export default function setCache(dockerIndex: DI, key: "docker.running", value: DockerInfo["running"]): R;
 export default function setCache(dockerIndex: DI, key: Keys, value: Values): R {
-  const nodeInfo = monitorInfoByDockerIndex[dockerIndex];
-  if (nodeInfo) {
+  const nodeCache = monitorInfoByDockerIndex[dockerIndex];
+  if (nodeCache) {
     switch (key) {
       case "docker.running":
-        nodeInfo.nodeInfo.docker.running = value as DockerInfo["running"];
+        nodeCache.nodeInfo.docker.running = value as DockerInfo["running"];
+        if (value) nodeCache.nodeInfo.docker.startedAt = new Date();
+        else nodeCache.nodeInfo.docker.finishedAt = new Date();
         break;
     }
-    return nodeInfo;
+    return nodeCache;
   }
 }
