78f231db342c439a48df8ab29e8bc306bb1e47b2
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-08-11 20:08:17 -0600

    Fix copyPendingBeacons: get pending nodes with beacon files

--- crons/copyPendingBeacons.ts
+++ crons/copyPendingBeacons.ts
@@ -2,11 +2,15 @@ import sortNodes from "../utils/sortNodes.ts";
 import { NodeStatus } from "../utils/getNodesStatus.ts";
 
 export default async function copyPendingBeacons() {
-  const { nodesStatusByDockerIndex } = await sortNodes(undefined, { fromCacheIfConvenient: true });
+  const { nodesStatusByDockerIndex, nodesInfoByDockerIndex } = await sortNodes(undefined, {
+    fromCacheIfConvenient: true,
+  });
+  const nodesInfo = Object.fromEntries(nodesInfoByDockerIndex);
 
   const pendingNodes = Object.values(nodesStatusByDockerIndex).filter(
-    (node) => node?.role === "PENDING"
+    (node) => node?.role === "PENDING" && nodesInfo[node.dockerIndex].beacon
   ) as NodeStatus[];
+
   if (pendingNodes.length <= 1) return;
 
   const firstNode = pendingNodes.shift()!.dockerIndex;
