f3bf814990de3429c6119ebd64835f2a9fd10edf
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-08-30 21:34:51 -0600

    Diffuser to load only the start and end time of every epoch

--- utils/diffuse.ts
+++ utils/diffuse.ts
@@ -1,11 +1,18 @@
 import axiod from "axiod";
-import msToTimeDescription from "./msToTimeDescription.ts";
+import { repeatUntilNoError } from "duplicatedFilesCleanerIncognito";
 
 export default async function diffuse() {
-  /** The key is the epoch */
+  function epochToHeight(epoch: number) {
+    return {
+      start: epoch * 350 + 1,
+      end: (epoch + 1) * 350,
+    };
+  }
+
   interface Data {
-    lastHeight: number;
-    data: Record<string, { height: number; time: number }[] | undefined>;
+    lastEpoch: number;
+    /** epoch: date */
+    data: Record<number | string, { start?: number; end?: number } | undefined>;
   }
 
   interface Result {
@@ -32,127 +39,35 @@ export default async function diffuse() {
     };
   }
 
-  const data: Data = (await Deno.readFile("data.json")
+  const data: Data = (await Deno.readFile("epochsDates.json")
     .then((x) => JSON.parse(new TextDecoder().decode(x)))
-    .catch(() => {})) || { lastHeight: 4045972, data: {} };
+    .catch(() => {})) || { lastEpoch: -1, data: {} };
 
-  function getMax(arr: number[]) {
-    let max = -Infinity;
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] > max) {
-        max = arr[i];
-      }
-    }
-    return max;
+  async function loadMore(height: number, isStart: boolean) {
+    const { epoch, time } = await repeatUntilNoError(() => getEpochAndTime(height || 1), 20, 1);
+    console.log(`Epoch: ${epoch}, Time: ${time}`);
+
+    const key = isStart ? "start" : "end";
+
+    if (!data.data[epoch]) data.data[epoch] = {};
+
+    if (data.data[epoch]![key]) throw new Error(`Epoch ${epoch} already has ${key} defined`);
+
+    data.data[epoch]![key] = +time;
   }
 
-  function getMin(arr: number[]) {
-    let min = Infinity;
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] < min) {
-        min = arr[i];
-      }
-    }
-    return min;
-  }
+  const simultaneous = 1;
 
-  // For epoches
-  console.log("Epoches");
-  {
-    const entries = Object.entries(data.data).filter(([, value]) => (value?.length || 0) === 350);
-    const ranges: number[] = [];
-    for (const [, data] of entries) {
-      if (!data) continue;
-      const maxDate = Math.max(...data.map((x) => x.time));
-      const minDate = Math.min(...data.map((x) => x.time));
-      ranges.push(maxDate - minDate);
-    }
-    console.table({
-      ["Total epoches"]: ranges.length,
-      ["Mean duration"]: msToTimeDescription(ranges.reduce((a, b) => a + b, 0) / ranges.length),
-      ["Max duration"]: msToTimeDescription(Math.max(...ranges)),
-      ["Min duration"]: msToTimeDescription(Math.min(...ranges)),
-    });
-
-    const rangesTimesCount: Record<string, number> = {};
-    for (const range of ranges) {
-      if (!rangesTimesCount[range]) rangesTimesCount[range] = 0;
-      rangesTimesCount[range]++;
-    }
-
-    console.table(
-      Object.fromEntries(
-        Object.entries(rangesTimesCount)
-          .sort(([a], [b]) => +a - +b)
-          .map(([range, count]) => [msToTimeDescription(+range, { short: true }), count])
-      )
-    );
-  }
-
-  // For blocks
-  console.log("\nBlocks");
-  {
-    const allBlocks = (Object.values(data.data).flat().filter(Boolean) as { height: number; time: number }[]).sort(
-      (a, b) => a.height - b.height
-    );
-    const ranges: number[] = [];
-
-    // check that all blocks are sequential and there are no blocks missing
-    for (let i = 0; i < allBlocks.length - 1; i++) {
-      if (allBlocks[i].height + 1 !== allBlocks[i + 1].height) {
-        console.error(`Missing blocks between ${allBlocks[i].height} and ${allBlocks[i + 1].height}`);
-      }
-    }
-
-    for (let i = 0; i < allBlocks.length - 1; i++) {
-      const block1 = allBlocks[i];
-      const block2 = allBlocks[i + 1];
-      ranges.push(block2.time - block1.time);
-    }
-
-    console.table({
-      ["Total blocks"]: allBlocks.length,
-      ["Mean duration"]: msToTimeDescription(ranges.reduce((a, b) => a + b, 0) / ranges.length),
-      ["Max duration"]: msToTimeDescription(getMax(ranges)),
-      ["Min duration"]: msToTimeDescription(getMin(ranges)),
-    });
-
-    const rangesTimesCount: Record<string, number> = {};
-    for (const range of ranges) {
-      if (!rangesTimesCount[range]) rangesTimesCount[range] = 0;
-      rangesTimesCount[range]++;
-    }
-
-    console.table(
-      Object.fromEntries(
-        Object.entries(rangesTimesCount)
-          .sort(([a], [b]) => +a - +b)
-          .map(([range, count]) => [msToTimeDescription(+range, { short: true }), count])
-      )
-    );
-  }
-
-  async function loadMore(height: number) {
-    const { epoch, time } = await getEpochAndTime(height);
-    console.log(`Height: ${height}, Epoch: ${epoch}, Time: ${time}`);
-
-    if (!data.data[epoch]) data.data[epoch] = [];
-    if (!data.data[epoch]!.find((x) => x.height === height))
-      data.data[epoch]!.push({ height, time: time.getTime() });
-  }
-
-  const simultaneous = 50;
   while (false) {
-    try {
-      await Promise.all(
-        Array.from({ length: simultaneous }, (_, i) => data.lastHeight - i).map((h) => loadMore(h))
-      );
+    await Promise.all(
+      Array.from({ length: simultaneous }, (_, i) => epochToHeight(data.lastEpoch + i + 1)).map(async (h) => {
+        await loadMore(h.start, true);
+        await loadMore(h.end, false);
+      })
+    );
 
-      data.lastHeight -= simultaneous;
+    data.lastEpoch += simultaneous;
 
-      await Deno.writeFile("data.json", new TextEncoder().encode(JSON.stringify(data, null, 2)));
-    } catch (e) {
-      console.error(e);
-    }
+    await Deno.writeFile("epochsDates.json", new TextEncoder().encode(JSON.stringify(data, null, 2)));
   }
 }
