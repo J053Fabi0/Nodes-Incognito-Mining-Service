fd8b81af3067be1fd809ca833f0096739f1dc7bb
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-08-30 22:33:51 -0600

    Diffuser to update nodes to get their epochs

--- utils/diffuse.ts
+++ utils/diffuse.ts
@@ -1,1 +1,38 @@
-export default async function diffuse() {}
+import { getNodes, changeNode } from "../controllers/node.controller.ts";
+import getBlockchainInfo from "../incognito/getBlockchainInfo.ts";
+
+export default async function diffuse() {
+  const info = await getBlockchainInfo();
+  console.log(info?.BestBlocks["-1"].Epoch);
+
+  interface Data {
+    lastEpoch: number;
+    /** epoch: date */
+    data: Record<number | string, { start?: number; end?: number } | undefined>;
+  }
+
+  const data: Data = await Deno.readFile("epochsDates.json").then((x) => JSON.parse(new TextDecoder().decode(x)));
+
+  const nodes = await getNodes();
+
+  const entries = Object.entries(data.data);
+
+  for (const node of nodes) {
+    if (typeof node.epoch === "number") continue;
+
+    const a = entries.find(([, data]) => {
+      if (!data) return false;
+      return data.start && data.start <= +node.createdAt && data.end && data.end >= +node.createdAt;
+    });
+
+    console.log(node.name, a ? a[0] : "No data");
+
+    let epoch = a ? +a[0] : null;
+    if (node.name === "82-64d475bf684cbb1cf670250a") epoch = 11313;
+
+    if (epoch !== null) {
+      await changeNode({ _id: node._id }, { $set: { epoch } });
+      console.log(`Node ${node.name} has been updated to epoch ${epoch}`);
+    }
+  }
+}
