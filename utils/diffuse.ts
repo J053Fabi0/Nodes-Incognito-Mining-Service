e4f2157a5c8a219edc33ed8172f2ee205304dc63
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-08-30 22:54:01 -0600

    Diffuser to delete earnings that are lower or equal to the node's epoch

--- utils/diffuse.ts
+++ utils/diffuse.ts
@@ -1,38 +1,26 @@
-import { getNodes, changeNode } from "../controllers/node.controller.ts";
-import getBlockchainInfo from "../incognito/getBlockchainInfo.ts";
+import { ObjectId } from "mongo/mod.ts";
+import { getNodes } from "../controllers/node.controller.ts";
+import { deleteNodeEarnings, getNodeEarnings } from "../controllers/nodeEarning.controller.ts";
 
 export default async function diffuse() {
-  const info = await getBlockchainInfo();
-  console.log(info?.BestBlocks["-1"].Epoch);
-
-  interface Data {
-    lastEpoch: number;
-    /** epoch: date */
-    data: Record<number | string, { start?: number; end?: number } | undefined>;
-  }
-
-  const data: Data = await Deno.readFile("epochsDates.json").then((x) => JSON.parse(new TextDecoder().decode(x)));
-
   const nodes = await getNodes();
 
-  const entries = Object.entries(data.data);
-
+  let totalDeleted = 0;
+  const allEarningsToDelete: ObjectId[] = [];
   for (const node of nodes) {
-    if (typeof node.epoch === "number") continue;
+    const earnings = await getNodeEarnings({ node: node._id });
 
-    const a = entries.find(([, data]) => {
-      if (!data) return false;
-      return data.start && data.start <= +node.createdAt && data.end && data.end >= +node.createdAt;
-    });
+    const earningsToDelete: ObjectId[] = [];
+    for (const earning of earnings) if (earning.epoch <= node.epoch) earningsToDelete.push(earning._id);
 
-    console.log(node.name, a ? a[0] : "No data");
-
-    let epoch = a ? +a[0] : null;
-    if (node.name === "82-64d475bf684cbb1cf670250a") epoch = 11313;
-
-    if (epoch !== null) {
-      await changeNode({ _id: node._id }, { $set: { epoch } });
-      console.log(`Node ${node.name} has been updated to epoch ${epoch}`);
+    if (earningsToDelete.length) {
+      console.log(`Deleting ${earningsToDelete.length} earnings from node ${node.name}`);
+      allEarningsToDelete.push(...earningsToDelete);
     }
+
+    totalDeleted += earningsToDelete.length;
   }
+
+  await deleteNodeEarnings({ _id: { $in: allEarningsToDelete } });
+  console.log("Total deleted:", totalDeleted);
 }
