572681fe7b9f6a11e9210866e720923398d82dfd
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-09-06 15:28:52 -0600

    Function to get the total unclaimed rewards of a node

---
+++ incognito/getNodeRewards.ts
@@ -0,0 +1,18 @@
+import axiod from "axiod";
+
+export default async function getNodeRewards(rewardAddress: string): Promise<number | null> {
+  const { data } = await axiod.post<{ Result: { PRV: number } | null; Error: null | { StackTrace: string } }>(
+    "https://mainnet.incognito.org/fullnode",
+    { jsonrpc: "1.0", method: "getrewardamount", params: [rewardAddress], id: 1 }
+  );
+
+  if (data.Error) {
+    if (data.Error.StackTrace.includes("Serialized key type is invalid"))
+      throw new Error("Serialized key type is invalid");
+    if (data.Error.StackTrace.includes("Serialized key is invalid")) throw new Error("Serialized key is invalid");
+
+    throw new Error(data.Error.StackTrace);
+  }
+
+  return data.Result?.PRV ?? null;
+}
