eaed2ae4b054bce8217297a3afee821575771e4c
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-09-08 17:11:44 -0600

    Diffuser to force update all dockers

--- utils/diffuse.ts
+++ utils/diffuse.ts
@@ -1,5 +1,8 @@
 import updateDockers from "../crons/updateDockers.ts";
 
 export default async function diffuse() {
-  await updateDockers();
+  await updateDockers({
+    force: true,
+    dockerIndexes: Array.from({ length: 324 }).map((_, i) => i + 1),
+  });
 }
