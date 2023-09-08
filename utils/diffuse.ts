8b8a66553a6367436150813ec0f99a30aa9ee7a6
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-09-08 17:53:08 -0600

    Update diffuser

--- utils/diffuse.ts
+++ utils/diffuse.ts
@@ -4,7 +4,7 @@ export default async function diffuse() {
   await updateDockers({
     force: true,
     dockerIndexes: Array.from({ length: 325 })
-      .map((_, i) => i + 1)
-      .slice(30),
+      .map((_, i) => i)
+      .slice(90),
   });
 }
