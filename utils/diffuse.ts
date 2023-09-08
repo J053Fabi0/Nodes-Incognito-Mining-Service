57969a21db0249595b999b3f5dc9d4eeac59bb11
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-09-08 17:19:45 -0600

    Update 3 nodes at the same time

--- utils/diffuse.ts
+++ utils/diffuse.ts
@@ -3,6 +3,8 @@ import updateDockers from "../crons/updateDockers.ts";
 export default async function diffuse() {
   await updateDockers({
     force: true,
-    dockerIndexes: Array.from({ length: 324 }).map((_, i) => i + 1),
+    dockerIndexes: Array.from({ length: 325 })
+      .map((_, i) => i + 1)
+      .slice(26),
   });
 }
