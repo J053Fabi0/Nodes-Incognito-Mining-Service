a032f650bb4e28b241d14c9828e97be915b451e9
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-09-08 21:35:49 -0600

    Diffuser: Added node to alreadyDone

--- utils/diffuse.ts
+++ utils/diffuse.ts
@@ -1,7 +1,7 @@
 import updateDockers from "../crons/updateDockers.ts";
 import sortNodes from "./sortNodes.ts";
 
-const alreadyDone = [147, 193, 269];
+const alreadyDone = [147, 193, 269, 206];
 
 export default async function diffuse() {
   const { nodesInfoByDockerIndex: nodesInfo } = await sortNodes(undefined, {
