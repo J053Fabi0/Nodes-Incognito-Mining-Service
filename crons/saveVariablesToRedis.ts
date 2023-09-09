7cefadf372eac244103f3004a8403fb2f3dcd276
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-09-09 10:28:01 -0600

    Added timeout to saveVariablesToRedis

--- crons/saveVariablesToRedis.ts
+++ crons/saveVariablesToRedis.ts
@@ -1,4 +1,6 @@
+import { sleep } from "sleep";
 import { redis } from "../initDatabase.ts";
+import handleError from "../utils/handleError.ts";
 import { variablesToSave } from "../utils/getRedisValue.ts";
 import { maxPromises } from "duplicatedFilesCleanerIncognito";
 
@@ -7,13 +9,22 @@ globalThis.addEventListener("unload", async () => {
   console.log("Saved variables to redis");
 });
 
+/** In seconds */
+const TIMEOUT = 10;
+
 export default async function saveVariablesToRedis() {
-  await maxPromises(
-    variablesToSave.map(
-      ([key, value]) =>
-        () =>
-          redis.set(key, JSON.stringify(value))
-    ),
-    3
-  );
+  let finished = false;
+  await Promise.race([
+    maxPromises(
+      variablesToSave.map(
+        ([key, value]) =>
+          () =>
+            redis.set(key, JSON.stringify(value))
+      ),
+      3
+    ).then(() => (finished = true)),
+    sleep(TIMEOUT).then(() => {
+      if (!finished) handleError(new Error(`saveVariablesToRedis timed out after ${TIMEOUT} seconds`));
+    }),
+  ]);
 }
