5c2ad042d2ced35a41f6124ac5582f274b940fab
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-09-08 19:06:46 -0600

    add repeatUntilNoError to getLatestTag

--- incognito/getLatestTag.ts
+++ incognito/getLatestTag.ts
@@ -1,12 +1,18 @@
 import axiod from "axiod";
+import repeatUntilNoError from "https://deno.land/x/duplicated_files_cleaner_incognito@3.1.6/utils/repeatUntilNoError.ts";
 
 interface Tag {
   name: string;
 }
 
 export default async function getLatestTag() {
-  const { data } = await axiod.get<{ results: Tag[] }>(
-    "https://hub.docker.com/v2/namespaces/incognitochain/repositories/incognito-mainnet/tags?page_size=1"
+  const { data } = await repeatUntilNoError(
+    async () =>
+      await axiod.get<{ results: Tag[] }>(
+        "https://hub.docker.com/v2/namespaces/incognitochain/repositories/incognito-mainnet/tags?page_size=1"
+      ),
+    5,
+    1
   );
 
   return data.results[0].name;
