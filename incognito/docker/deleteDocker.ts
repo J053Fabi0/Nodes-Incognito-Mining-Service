402b1402e3ea73937673a40d11244e46004c74df
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-09-09 10:39:03 -0600

    Ignore errors when trying to stop docker when deleting them

--- incognito/docker/deleteDocker.ts
+++ incognito/docker/deleteDocker.ts
@@ -7,7 +7,7 @@ import doesDirExists from "../../utils/doesDirExists.ts";
  * Stops and removes a docker container. Removes also the data directory by default.
  */
 export default async function deleteDocker(dockerIndex: number, deleteDataDir = true) {
-  await docker(["stop", `inc_mainnet_${dockerIndex}`]).catch(handleError);
+  await docker(["stop", `inc_mainnet_${dockerIndex}`]).catch(() => {});
   await docker(["rm", `inc_mainnet_${dockerIndex}`]).catch(handleError);
   if (deleteDataDir && (await doesDirExists(`${dataDir}_${dockerIndex}`)))
     await Deno.remove(`${dataDir}_${dockerIndex}`, { recursive: true }).catch(handleError);
