156b735c9c1c187061ffa8103f2e3e0f4275f8ad
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-09-09 10:35:42 -0600

    Check if dir exists before trying to delete it

--- incognito/docker/deleteDocker.ts
+++ incognito/docker/deleteDocker.ts
@@ -1,6 +1,7 @@
 import { dataDir } from "./createDocker.ts";
 import { docker } from "../../utils/commands.ts";
 import handleError from "../../utils/handleError.ts";
+import doesDirExists from "../../utils/doesDirExists.ts";
 
 /**
  * Stops and removes a docker container. Removes also the data directory by default.
@@ -8,5 +9,6 @@ import handleError from "../../utils/handleError.ts";
 export default async function deleteDocker(dockerIndex: number, deleteDataDir = true) {
   await docker(["stop", `inc_mainnet_${dockerIndex}`]).catch(handleError);
   await docker(["rm", `inc_mainnet_${dockerIndex}`]).catch(handleError);
-  if (deleteDataDir) await Deno.remove(`${dataDir}_${dockerIndex}`, { recursive: true }).catch(handleError);
+  if (deleteDataDir && (await doesDirExists(`${dataDir}_${dockerIndex}`)))
+    await Deno.remove(`${dataDir}_${dockerIndex}`, { recursive: true }).catch(handleError);
 }
