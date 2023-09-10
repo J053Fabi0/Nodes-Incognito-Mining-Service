384fdff5f19aa1ba7b6b4d13510801e7f45bcaee
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-09-09 19:11:16 -0600

    Diffuser to test the times it takes to save to redis

--- utils/diffuse.ts
+++ utils/diffuse.ts
@@ -1,19 +1,39 @@
-import sortNodes from "./sortNodes.ts";
-import updateDockers from "../crons/updateDockers.ts";
+// import sortNodes from "./sortNodes.ts";
+// import updateDockers from "../crons/updateDockers.ts";
 
-const alreadyDone = [147, 193, 269, 206];
+import { lodash } from "lodash";
+import { times } from "../crons/saveVariablesToRedis.ts";
+import msToTimeDescription from "./msToTimeDescription.ts";
+import { sendHTMLMessage } from "../telegram/sendMessage.ts";
+
+// const alreadyDone = [147, 193, 269, 206];
 
 export default async function diffuse() {
-  const { nodesInfoByDockerIndex: nodesInfo, nodesStatusByDockerIndex: nodeStatus } = await sortNodes(undefined, {
-    fullData: false,
-    fromCacheIfConvenient: true,
-  });
+  const totalTime = times.reduce((a, b) => a + b, 0);
+  const averageTime = totalTime / times.length;
+  const maxTime = lodash.max(times) ?? 0;
+  const minTime = lodash.min(times) ?? 0;
+  const options: Parameters<typeof msToTimeDescription>[1] = { short: true, includeMs: true };
+  await sendHTMLMessage(
+    "<code>" +
+      `Average time: ${msToTimeDescription(averageTime, options)}.\n` +
+      `Max time:     ${msToTimeDescription(maxTime, options)}.\n` +
+      `Min time:     ${msToTimeDescription(minTime, options)}.\n` +
+      `Total time:   ${msToTimeDescription(totalTime, options)}.\n` +
+      `Total times:  ${times.length}.` +
+      "</code>"
+  );
 
-  await updateDockers({
-    force: true,
-    dockerIndexes: nodesInfo
-      .filter(([n]) => nodeStatus[n]?.role !== "COMMITTEE")
-      .map(([n]) => +n)
-      .filter((n) => !alreadyDone.includes(n)),
-  });
+  // const { nodesInfoByDockerIndex: nodesInfo, nodesStatusByDockerIndex: nodeStatus } = await sortNodes(undefined, {
+  //   fullData: false,
+  //   fromCacheIfConvenient: true,
+  // });
+
+  // await updateDockers({
+  //   force: true,
+  //   dockerIndexes: nodesInfo
+  //     .filter(([n]) => nodeStatus[n]?.role !== "COMMITTEE")
+  //     .map(([n]) => +n)
+  //     .filter((n) => !alreadyDone.includes(n)),
+  // });
 }
