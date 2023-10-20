3c992418f4cb14d9571dcd34f2ba49600bf6ce34
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-10-19 19:22:46 -0600

    Code to supress the error "Improper nesting of table"

--- dev.ts
+++ dev.ts
@@ -3,4 +3,11 @@
 import "./crons/crons.ts";
 import dev from "$fresh/dev.ts";
 
+// Code to supress the error "Improper nesting of table"
+const origConsoleError = console.error;
+console.error = (msg) => {
+  if (typeof msg === "string" && msg.includes("Improper nesting of table")) return;
+  origConsoleError(msg);
+};
+
 await dev(import.meta.url, "./main.ts");
