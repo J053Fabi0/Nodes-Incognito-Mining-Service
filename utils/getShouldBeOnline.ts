51cc8b14a0aa08f0bd45605fbbcb12d64f46d70f
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-12-26 14:01:47 -0600

    Revert "Do not have every node with beacon online, only the ones that should be online"
    This reverts commit bc37baea020a0554729cc6e1fd5cc50d316d84b2.

--- utils/getShouldBeOnline.ts
+++ utils/getShouldBeOnline.ts
@@ -1,14 +1,38 @@
 import { onlineQueue } from "./variables.ts";
-import { NodeStatus } from "./getNodesStatus.ts";
+import { NodeInfoByDockerIndex } from "./sortNodes.ts";
+import { NodeRoles, NodeStatus } from "./getNodesStatus.ts";
 import { maxOnlineNodesNotStaked, minEpochsToBeOnlinePending, minEpochsToBeOnlineSyncing } from "../constants.ts";
 
 type PartialNodeStatus = Partial<NodeStatus> & Pick<NodeStatus, "epochsToNextEvent" | "role" | "dockerIndex">;
 
+const rolesThatUseSortOrder: NodeRoles[] = ["PENDING", "SYNCING"];
+
 export default function getShouldBeOnline(nodeStatus: PartialNodeStatus): boolean;
-export default function getShouldBeOnline(nodeStatus: PartialNodeStatus): boolean {
+export default function getShouldBeOnline(
+  nodeStatus: PartialNodeStatus,
+  maxNodesOnline: number,
+  nodesInfoByDockerIndex: NodeInfoByDockerIndex[]
+): boolean;
+export default function getShouldBeOnline(
+  nodeStatus: PartialNodeStatus,
+  maxNodesOnline?: number,
+  nodesInfoByDockerIndex?: NodeInfoByDockerIndex[]
+): boolean {
   // committee nodes are always online
   if (nodeStatus.role === "COMMITTEE") return true;
 
+  const useSortOrder = maxNodesOnline !== undefined && nodesInfoByDockerIndex !== undefined;
+
+  if (useSortOrder && rolesThatUseSortOrder.includes(nodeStatus.role)) {
+    const index = nodesInfoByDockerIndex.findIndex(([index]) => +index === nodeStatus.dockerIndex);
+    if (index === -1) return false;
+    return index + 1 <= maxNodesOnline;
+  }
+  //
+  else return switchMethod(nodeStatus);
+}
+
+function switchMethod(nodeStatus: PartialNodeStatus) {
   switch (nodeStatus.role) {
     case "PENDING":
       return nodeStatus.epochsToNextEvent <= minEpochsToBeOnlinePending;
